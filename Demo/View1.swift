aa19d3d15e7f345fde59026eca5be97bf1c6e887
yazhi <zengyazhiwork@163.com>
2020-02-07 21:07:53 +0800

    remove useless code

---
+++ Demo/View1.swift
@@ -0,0 +1,20 @@
+//
+//  View1.swift
+//  Demo
+//
+//  Created by zengyazhi on 2020/1/6.
+//  Copyright © 2020 zengyazhi. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class View1: UIViewController {
+    override func viewDidLoad() {
+        self.title = "Demo"
+        self.view.backgroundColor = UIColor.white
+//        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.edgesForExtendedLayout = []
+
+    }
+}
--- Podfile
+++ Podfile
@@ -1,8 +1,7 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-# source 'https://github.com/CocoaPods/Specs.git'
-source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git'
+source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '10.0'
 use_frameworks!
 
